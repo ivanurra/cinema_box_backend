58d47c3acd7597db81d792080af71d7d42f78e90
Ivan Urra <ivan.urra.wd@gmail.com>
2020-12-09 13:15:30 +0100

    backend ready

--- models/User.js
+++ models/User.js
@@ -15,7 +15,6 @@ const userSchema = new Schema({
       type: String,
       required:true
   },
-  comment: [],
   favoriteMovies: [],
   favoriteSeries:[]
 }, {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 
-const Comment = require('../models/Comment')
 const Movie = require("../models/Movie")
 const User = require('../models/User')
 
@@ -15,7 +14,6 @@ router.get('/', (req, res, next) => {
 //   // Base URLS
 //   app.use('/api', require('./movies-routes.js'))
 //   app.use('/api', require('./auth-routes.js'))
-//   app.use('/api', require('./coment-routes.js'))
 // }
 
 module.exports = router;
